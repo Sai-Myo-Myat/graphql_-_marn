1453082739a1429e4ddb4bae0cf49f9475a542e3
Sai-Myo-Myat <saimyomyat511@gmail.com>
2023-02-02 09:40:06 +0630

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,12 @@
+import {ApolloProvider,ApolloClient,InMemoryCache} from "@apollo/client"
+
 import Header from "./components/Header"
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000",
+  
+})
 
 function App() {
 
